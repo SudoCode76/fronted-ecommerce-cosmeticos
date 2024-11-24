15eed508a389f1f77df1e2c6d2b2473c523131b4
SudoCode76 <bo3321319@gmail.com>
2024-11-23 20:29:49 -0400

    footer terminado

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <NavBar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
